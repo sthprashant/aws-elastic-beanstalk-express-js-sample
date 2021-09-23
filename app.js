82d296e076fcd2fb064ea162a24b63f789bbfa2d
Prashant Shrestha <sthprashant@gmail.com>
2021-09-24 00:38:39 +1000

    pipeline created and activated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS Test!'));
+app.get('/', (req, res) => res.send('Pipeline is created using AWS CodePipline, CodeBuild, ElasticBeanstalk'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
